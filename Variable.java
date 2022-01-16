57b469962638b20ecab90454cc5b82b726f82a38
Rei Rich <r.rich@electroneek.com>
2022-01-16 23:35:05 +0300

    MyFirstApp and Variable are added

---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte processor = 64;
+		short ssdGb = 233;
+		int productCode = 99999;
+		long freeBytes = 146841776128L;
+		float RAM = 12.0f;
+		double freeRAM = 9.794000002;
+		char operationSystem = 'W';
+		boolean isActive = true;
+
+		System.out.println("Processor: "+ processor);
+		System.out.println("SSD GB: " + ssdGb);
+		System.out.println("Product code: " + productCode);
+		System.out.println("Free bytes: " + freeBytes);
+		System.out.println("RAM: " + RAM);
+		System.out.println("Free RAM: " + freeRAM);
+		System.out.println("Operation system: " + operationSystem);
+		System.out.println("Active status: " + isActive);
+	}
+}
