033fb8a2456d9f5175426f9855cdfc48f750ccc7
Rei Rich <r.rich@electroneek.com>
2022-01-19 21:54:14 +0300

    Replaced variables

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,13 +1,12 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
-        byte age = (byte) 99;
-        boolean isMale = false;
-        float height = 1.60f;
-        char nameFirstLetter = 'D';
+        byte age = 99;
 
         if(age > 20) {
             System.out.println("Теперь можно всё.");
         }
+
+        boolean isMale = false;
 
         if(isMale) {
             System.out.println("Мужчина.");
@@ -17,11 +16,15 @@ public class ConditionalStatement {
             System.out.println("Женщина.");
         }
 
+        float height = 1.60f;
+
         if(height < 1.80) {
             System.out.println("Не ел растишку.");
         } else {
             System.out.println("Прирождённый баскетболист.");
         }
+
+        char nameFirstLetter = 'D';
 
         if(nameFirstLetter == 'M') {
             System.out.println("Марк Твен?");            
